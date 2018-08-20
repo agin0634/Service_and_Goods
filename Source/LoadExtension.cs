80291759101716c457a0f4887fa7894dffae91cf
Duri Shen <agin0511@gmail.com>
2018-08-20 19:36:05 +0800

    remove pollution event

--- Source/LoadExtension.cs
+++ Source/LoadExtension.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using ICities;
+using ColossalFramework;
 using ColossalFramework.Plugins;
 using UnityEngine;
 
@@ -10,11 +11,15 @@ namespace Service_and_Goods
     public class LoadExtension : ThreadingExtensionBase
     {
         Service_and_GoodsConfiguration Config = Configuration<Service_and_GoodsConfiguration>.Load();
-        public static LoadExtension Instance { get; protected set; }
+        //public static LoadExtension instance { get; protected set; }
+
+        private uint PreviousFrameIndex = 0;
+        private uint FrameIndexThreshold = 60;
+        
 
         public override void OnCreated(IThreading threading)
         {
-            Instance = this;
+            //instance = this;
             base.OnCreated(threading);
             DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "on created");
         }
@@ -22,19 +27,50 @@ namespace Service_and_Goods
         public override void OnReleased()
         {
             base.OnReleased();
-            Instance = null;
+           // instance = null;
             DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "on released");
         }
+        /*
+        public static bool InGame
+        {
+            get
+            {
+                var loading = instance.managers.loading;
+                if (loading == null) return false;
+                return loading.loadingComplete && (loading.currentMode == AppMode.Game);
+            }
+        }*/
 
         public override void OnAfterSimulationFrame()
         {
-            //TODO logic
+           /* if (!InGame) {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "HI");
+                return;
+            }*/
+           
 
-            if (Config.RemovePollutionGround)
+            if (Singleton<DistrictManager>.exists &&
+                !SimulationManager.instance.SimulationPaused &&
+                SimulationManager.instance.m_currentFrameIndex - PreviousFrameIndex > FrameIndexThreshold)
             {
-                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Remove Ground on");
-            }
-            base.OnAfterSimulationFrame();
+                //DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "HI");
+                if (Config.RemovePollutionGround)
+                {
+                    Singleton<NaturalResourceManager>.instance.AddPollutionDisposeRate(10000);
+                }
+                if (Config.RemovePollutionWater)
+                {
+                    Singleton<TerrainManager>.instance.WaterSimulation.AddPollutionDisposeRate(10000);
+                }
+                if (Config.RemovePollutionNoise)
+                {
+                    ImmaterialResourceManager.instance.AddResource(ImmaterialResourceManager.Resource.NoisePollution, -10000);
+                }
+                PreviousFrameIndex = SimulationManager.instance.m_currentFrameIndex;
+            };
+
+
+            base.OnAfterSimulationTick();
         }
     }
 }
